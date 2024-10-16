f4abe4e336924e8e7d0ba8244a36c68aa150937f
Eric Chen <eric.chen@lab3nz.co.nz>
2024-10-17 12:33:16 +1300

    cli resource deploy

---
+++ cli_deploy/providers.tf
@@ -0,0 +1,5 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "2495078d-1239-41f1-8e49-991b1b8ba226"
+}
---
+++ cli_deploy/rg.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "prg-aue-identity-01"
+  location = "Australia East"
+}
+
